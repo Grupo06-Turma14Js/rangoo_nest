09dcc3d726c8941dca35a9332344dc59df24732b
Alice Tolosa <aliceoliveira81@outlook.com>
2026-04-17 10:58:35 -0300

    feat: inicialização do projeto e criação da entidade produto

---
+++ src/produto/entities/produto.entity.ts
@@ -0,0 +1,31 @@
+import { IsNotEmpty } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name:"tb_produtos"}) //Criação da tabela
+export class Produto {
+
+   
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @IsNotEmpty()
+  @Column({length: 100, nullable: false})
+  nome!: string;
+
+  @IsNotEmpty()
+  @Column({length: 1000, nullable: false})
+  descricao!: string;
+
+  @IsNotEmpty()
+  @Column({type: 'decimal',precision:10, scale: 2, nullable: false})
+  preco!: number;
+
+  @IsNotEmpty()
+  @Column({ default: true })
+  ativo!: boolean;
+
+  
+  //Relacionamento PENDENTE 
+  
+  
+}
