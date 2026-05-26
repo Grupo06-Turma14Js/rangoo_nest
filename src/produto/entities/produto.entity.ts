7e55bea0a95d6fdddd854510a74c6995a1c0c719
Marlon Panerari <marpan.dev@gmail.com>
2026-05-25 22:34:06 -0300

    🐛 fix: objetivos entity

--- src/produto/entities/produto.entity.ts
+++ src/produto/entities/produto.entity.ts
@@ -7,6 +7,7 @@ import { ApiProperty } from "@nestjs/swagger";
 export enum Objetivo {
   EMAGRECIMENTO = "emagrecimento",
   GANHO_MASSA = "ganho-massa",
+  VEGETARIANO   = "vegetariano",
   DIABETICO = "diabetico",
   SEM_LACTOSE = "sem-lactose",
   SEM_GLUTEN = "sem-gluten",
