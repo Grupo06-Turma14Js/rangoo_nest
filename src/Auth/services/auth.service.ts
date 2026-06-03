f3cc2c34fa339444ab877eea53a54d4613fd929b
Isabela <isabela_siqueirasilva@hotmail.com>
2026-06-03 14:00:47 -0300

    feat: Implementação do login com JWT

--- src/Auth/services/auth.service.ts
+++ src/Auth/services/auth.service.ts
@@ -47,6 +47,7 @@ export class AuthService {
             usuario: usuarioLogin.usuario,
             senha: '',
             foto: buscaUsuario.foto,
+            tipo: buscaUsuario.tipo,
             token: `Bearer ${this.jwtService.sign(payload)}`,
         }
     }
